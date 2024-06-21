a401ea9ce575d6475449b8029e9cfacd804efd25
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-06-20 22:33:02 -0300

    add login for staff by jwt

--- src/controllers/controllerNewStaff.ts
+++ src/controllers/controllerNewStaff.ts
@@ -53,24 +53,85 @@ export const updateStaff = async (req: Request, res: Response) => {
 
 export const registerNewStaff = async (req: Request, res: Response) => {
   try {
-    const body = req.body;
+    const { nome, cartao, setor, password } = req.body;
 
-    if (body) {
-      const statusRegisterNewStaff = await prisma.staff.create({
-        data: body,
+    if (req.body) {
+      const existingStaff = await prisma.staff.findUnique({
+        where: {
+          cartao,
+        },
       });
 
-      if (statusRegisterNewStaff) {
-        console.log(statusRegisterNewStaff);
-        res.status(200).json({
-          warning: 'registrado com sucesso!',
+      if (existingStaff)
+        return res.status(409).json({
+          warning: 'Usuário já existente!',
         });
-      }
+
+      const salt = await bcrypt.genSalt(10);
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      const registerNewStaff = await prisma.staff.create({
+        data: {
+          nome,
+          cartao,
+          setor,
+          passwordHash,
+        },
+      });
+
+      res.status(201).json({
+        warning: 'Staff criado com sucesso',
+        user: registerNewStaff,
+      });
     }
   } catch (error) {
     console.log('Erro ao criar registrar staff', error);
     res.status(500).json({
       error: 'Erro ao criar registrar staff',
+    });
+  }
+};
+
+export const loginStaff = async (req: Request, res: Response) => {
+  try {
+    const { nome, cartao, setor, password } = req.body;
+
+    if (req.body) {
+      const staff = await prisma.staff.findUnique({
+        where: {
+          cartao,
+        },
+      });
+
+      if (!staff) {
+        return res.status(404).json({
+          warning: 'Staff não encontrado',
+        });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, staff.passwordHash);
+
+      if (!passwordMatch)
+        return res.status(401).json({ warning: 'credenciais incorreta' });
+
+      const token = jwt.sign(
+        {
+          id: staff.id,
+          cartao,
+        },
+        'secrete',
+        { expiresIn: '1h' }
+      );
+
+      res.status(200).json({
+        token,
+        staff,
+      });
+    }
+  } catch (error) {
+    console.log('Erro ao realizar login', error);
+    res.status(500).json({
+      error: 'Erro ao realizar login',
     });
   }
 };
