c6ad1ff53034991b6518af77aaccb87ed1917e08
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-07-08 18:59:40 -0300

    add new sentence for verification if req.headers exist

--- src/middleware/checkToken.ts
+++ src/middleware/checkToken.ts
@@ -7,32 +7,38 @@ const prisma = new PrismaClient();
 export const verifyToken = async (req: Request, res: Response) => {
   try {
     console.log('teste');
-    const token = req.headers.authorization?.split(' ')[1];
+    if (req.headers.authorization) {
+      const token = req.headers.authorization.split(' ')[1];
 
-    if (!token) {
-      return res.status(401).json({ error: 'Token não fornecido' });
+      if (!token) {
+        return res.status(401).json({ error: 'Token não fornecido' });
+      }
+      const decoded = jwt.verify(token, 'secrete') as {
+        id: number;
+        cartao: string;
+      };
+
+      const user = await prisma.staff.findUnique({
+        where: {
+          id: decoded.id,
+          cartao: decoded.cartao,
+        },
+      });
+
+      if (!user) {
+        return res.status(404).json({ error: 'Usuário não encontrado' });
+      }
+
+      res.status(200).json({
+        id: user.id,
+        nome: user.nome,
+        cartao: user.cartao,
+      });
+    } else {
+      res.status(500).json({
+        error: 'token nor found',
+      });
     }
-    const decoded = jwt.verify(token, 'secrete') as {
-      id: number;
-      cartao: string;
-    };
-
-    const user = await prisma.staff.findUnique({
-      where: {
-        id: decoded.id,
-        cartao: decoded.cartao,
-      },
-    });
-
-    if (!user) {
-      return res.status(404).json({ error: 'Usuário não encontrado' });
-    }
-
-    res.status(200).json({
-      id: user.id,
-      nome: user.nome,
-      cartao: user.cartao,
-    });
   } catch (error) {
     console.error('Erro ao verificar token', error);
     res.status(500).json({ error: 'Erro ao verificar token' });
