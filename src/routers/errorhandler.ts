8c7cd467da6652e1ea6076e7f4341f3adca86edd
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-07-08 19:22:29 -0300

    alter running server

---
+++ src/routers/errorhandler.ts
@@ -0,0 +1,13 @@
+import express, { ErrorRequestHandler, RequestHandler } from 'express';
+export const notFoundPage: RequestHandler = (req, res) => {
+  res.status(404).json({
+    error: 'page not found',
+  });
+};
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    error: 'erro interno no servidor',
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import helmet from 'helmet';
 import { apiRouter } from './routers/router';
+import { errorHandler, notFoundPage } from './routers/errorhandler';
+
+const SERVER: string = process.env.IP_SERVER || '127.0.0.1';
+const PORT: number =
+  typeof process.env.PORT === 'string' ? +process.env.PORT : 8080 || 8080;
 
 const server = express();
 
 server.use(cors());
+server.use(helmet());
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, '../public')));
 
 server.use('/api', apiRouter);
+server.use(notFoundPage);
+server.use(errorHandler);
 
 server.use((req, res) => {
   res.status(404).json({
@@ -15,6 +27,7 @@ server.use((req, res) => {
   });
 });
 
-server.listen(8080, () => {
-  console.log('server running');
+server.listen(PORT, SERVER, () => {
+  console.log(SERVER, PORT);
+  console.log('server running!');
 });
