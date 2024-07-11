d66aca106ba17ce9feebe9738e9d21f3c40bbc07
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-07-10 21:13:09 -0300

    add new authentification and alter my schema

--- src/middleware/checkSetor.ts
+++ src/middleware/checkSetor.ts
@@ -1,13 +1,44 @@
 import { PrismaClient } from '@prisma/client';
-import { NextFunction, Request, Response } from 'express';
+import { RequestHandler } from 'express';
 
 const prisma = new PrismaClient();
 
-export const checkAuthorizedSetor = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+export const checkAuthorizedForAmbulatory: RequestHandler = async (
+  req,
+  res,
+  next
 ) => {
+  try {
+    const { userId } = req.params;
+
+    if (userId) {
+      const user = await prisma.staff.findUnique({
+        where: {
+          id: +userId,
+        },
+        select: {
+          setor: true,
+        },
+      });
+
+      if (!user)
+        return res.status(400).json({
+          warning: 'você precisa estar logado para acessar essa area!',
+        });
+      if (user.setor !== 'ambulatorio')
+        return res.status(401).json({ warning: 'acesso não autorizado' });
+
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: 'erro interno ao verificar setor autorizado',
+    });
+  }
+};
+
+export const checkAuthorizedSetor: RequestHandler = async (req, res, next) => {
   try {
     const { userId } = req.params;
 
