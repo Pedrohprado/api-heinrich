4d73bf4badb6ffe0f9d76d1679ba960db0db23c3
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-07-23 15:07:30 -0300

    add new fuction for delete individual img

--- src/controllers/controllerImagens.ts
+++ src/controllers/controllerImagens.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
+import { RequestHandler } from 'express';
 import { prisma } from '../services/prisma';
 
 export const deleteManyImg = async (id: number) => {
@@ -24,4 +25,28 @@ export const deleteManyImg = async (id: number) => {
       registerId: id,
     },
   });
+};
+
+export const deleteImageById: RequestHandler = async (req, res) => {
+  try {
+    const { imageId } = req.params;
+
+    if (imageId) {
+      const statusDelete = await prisma.imagens.delete({
+        where: {
+          id: +imageId,
+        },
+      });
+      if (statusDelete) {
+        fs.unlinkSync(
+          path.join(__dirname, '..', './public', statusDelete.path)
+        );
+
+        res.status(200).json({ warning: 'imagem deletada!' });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ warning: 'erro interno do servidor' });
+    console.log(error);
+  }
 };
